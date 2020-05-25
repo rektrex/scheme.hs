610bff8281a7fc7cc517bbf0c2219a6c8f580c95
rektrex <23721797+rektrex@users.noreply.github.com>
2020-05-25 20:06:06 +0530

    datatypeparser

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.Environment
+import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 symbol :: Parser Char
@@ -10,9 +11,41 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
                    Left err -> "No match: " ++ show err
                    Right val -> "Found value"
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = [first] ++ rest
+  return $ case atom of
+             "#t" -> Bool True
+             "#f" -> Bool False
+             otherwise -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
 
 main :: IO ()
 main = do
